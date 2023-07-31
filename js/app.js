7d087956c384976a3437260dc8d8145f488d92a1
Cecilia Do Nascimento <mcd.cecilia.donascimento@gmail.com>
2023-07-31 01:25:12 -0300

    Ejrcicio A

---
+++ js/app.js
@@ -0,0 +1,20 @@
+console.log("Ejercicio A");
+//😺 😸 😹 🐈 🐾 ⬛
+
+
+
+for (var cantidadDeGatos = 1; cantidadDeGatos <= 10; cantidadDeGatos++) {
+    if (cantidadDeGatos % 3 === 1) {
+        console.log("Gato #" + cantidadDeGatos + ": 😺");
+    } else if (cantidadDeGatos % 3 === 2) {
+        console.log("Gato #" + cantidadDeGatos + ": 😸");
+    } else {
+        console.log("Gato #" + cantidadDeGatos + ": 😹");
+    }
+}
+
+
+
+
+
+
